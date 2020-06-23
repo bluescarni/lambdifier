46b085e37ba644d4a578cde82fc18046e9664879
Francesco Biscani <bluescarni@gmail.com>
2020-06-23 09:58:04 +0200

    WIP checkpoint.

--- src/llvm_state.cpp
+++ src/llvm_state.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdint>
+#include <initializer_list>
 #include <limits>
 #include <memory>
 #include <stdexcept>
@@ -13,6 +14,7 @@
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/InstrTypes.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
@@ -22,10 +24,13 @@
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/Error.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/IPO.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
 #include <llvm/Transforms/InstCombine/InstCombine.h>
 #include <llvm/Transforms/Scalar.h>
 #include <llvm/Transforms/Scalar/GVN.h>
 #include <llvm/Transforms/Utils.h>
+#include <llvm/Transforms/Vectorize.h>
 
 #include <lambdifier/detail/check_symbol_name.hpp>
 #include <lambdifier/expression.hpp>
@@ -34,7 +39,7 @@
 namespace lambdifier
 {
 
-llvm_state::llvm_state(const std::string &name)
+llvm_state::llvm_state(const std::string &name, unsigned l) : opt_level(l)
 {
     // Create the module.
     module = std::make_unique<llvm::Module>(name, get_context());
@@ -42,25 +47,26 @@ llvm_state::llvm_state(const std::string &name)
 
     // Create a new builder for the module.
     builder = std::make_unique<llvm::IRBuilder<>>(get_context());
-    // Set a couple of flags for faster math at the
-    // price of potential change of semantics.
-    llvm::FastMathFlags fmf;
-    // fmf.setAllowReciprocal();
-    // fmf.setAllowReassoc();
-    // fmf.setNoNaNs();
-    // fmf.setNoInfs();
-    fmf.setFast();
-    builder->setFastMathFlags(fmf);
+    if (opt_level >= 3u) {
+        // Set a couple of flags for faster math at the
+        // price of potential change of semantics.
+        llvm::FastMathFlags fmf;
+        fmf.setFast();
+        builder->setFastMathFlags(fmf);
+    }
 
     // Create the function pass manager.
     fpm = std::make_unique<llvm::legacy::FunctionPassManager>(module.get());
-    // TODO tunable param.
-    for (auto i = 0; i < 3; ++i) {
-        fpm->add(llvm::createPromoteMemoryToRegisterPass());
-        fpm->add(llvm::createInstructionCombiningPass());
-        fpm->add(llvm::createReassociatePass());
-        fpm->add(llvm::createGVNPass());
-        fpm->add(llvm::createCFGSimplificationPass());
+    for (auto i = 0u; i < opt_level; ++i) {
+        // fpm->add(llvm::createPromoteMemoryToRegisterPass());
+        // fpm->add(llvm::createInstructionCombiningPass());
+        // fpm->add(llvm::createReassociatePass());
+        // fpm->add(llvm::createGVNPass());
+        // fpm->add(llvm::createCFGSimplificationPass());
+        // fpm->add(llvm::createLoopVectorizePass());
+        // fpm->add(llvm::createSLPVectorizerPass());
+        // fpm->add(llvm::createLoadStoreVectorizerPass());
+        // fpm->add(llvm::createLoopUnrollPass());
     }
     fpm->doInitialization();
 }
@@ -111,7 +117,7 @@ void llvm_state::optimize_function(llvm::Function *f)
     fpm->run(*f);
 }
 
-void llvm_state::add_varargs_expression(const std::string &name, const expression &e, bool optimize,
+void llvm_state::add_varargs_expression(const std::string &name, const expression &e,
                                         const std::vector<std::string> &vars)
 {
     // Prepare the function prototype. First the function arguments.
@@ -147,16 +153,14 @@ void llvm_state::add_varargs_expression(const std::string &name, const expressio
         verify_function(f);
 
         // Optimize it.
-        if (optimize) {
-            optimize_function(f);
-        }
+        optimize_function(f);
     } else {
         // Error reading body, remove function.
         f->eraseFromParent();
     }
 }
 
-void llvm_state::add_vecargs_expression(const std::string &name, bool optimize, const std::vector<std::string> &vars)
+void llvm_state::add_vecargs_expression(const std::string &name, const std::vector<std::string> &vars)
 {
     // NOTE: we support indices within the unsigned range
     // below (when using the CreateConstInBoundsGEP1_32()
@@ -243,6 +247,8 @@ void llvm_state::add_vecargs_expression(const std::string &name, bool optimize, 
 
     // Do the invocation.
     if (auto *ret_val = builder->CreateCall(varargs_f, args_v, "calltmp")) {
+        ret_val->setTailCall(true);
+
         // Finish off the function.
         builder->CreateRet(ret_val);
 
@@ -250,16 +256,152 @@ void llvm_state::add_vecargs_expression(const std::string &name, bool optimize, 
         verify_function(f);
 
         // Optimize it.
-        if (optimize) {
-            optimize_function(f);
-        }
+        optimize_function(f);
     } else {
         // Error reading body, remove function.
         f->eraseFromParent();
     }
 }
 
-void llvm_state::add_expression(const std::string &name, const expression &e, bool optimize)
+void llvm_state::add_batch_expression(const std::string &name, const std::vector<std::string> &vars,
+                                      unsigned batch_size)
+{
+    // NOTE: we support indices within the unsigned range below.
+    if (vars.size() > std::numeric_limits<unsigned>::max()) {
+        throw std::overflow_error("The number of variables in an expression, " + std::to_string(vars.size())
+                                  + ", is too large");
+    }
+
+    // Prepare the function prototype. Two pointers, one out, one in.
+    std::vector<llvm::Type *> fargs(2, llvm::PointerType::getUnqual(builder->getDoubleTy()));
+
+    // Then the return type.
+    auto *ft = llvm::FunctionType::get(builder->getVoidTy(), fargs, false);
+    assert(ft != nullptr);
+
+    // Now create the function.
+    auto *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, name + ".batch", module.get());
+    assert(f != nullptr);
+
+    // Set the name of the function arguments.
+    const auto arg_rng = f->args();
+    assert(arg_rng.begin() != arg_rng.end() && arg_rng.begin() + 2 == arg_rng.end());
+
+    // The output argument.
+    auto out_arg = arg_rng.begin();
+    out_arg->setName("batcharg.out");
+    // Specify that this is a write-only pointer argument.
+    out_arg->addAttr(llvm::Attribute::WriteOnly);
+    // Specify that the function does not make any copies of the
+    // pointer argument that outlive the function itself.
+    out_arg->addAttr(llvm::Attribute::NoCapture);
+    // No aliasing is allowed on the pointer arguments
+    // for this function.
+    out_arg->addAttr(llvm::Attribute::NoAlias);
+
+    // The input argument.
+    auto in_arg = out_arg + 1;
+    in_arg->setName("batcharg.in");
+    // Specify that this is a read-only pointer argument.
+    in_arg->addAttr(llvm::Attribute::ReadOnly);
+    // Specify that the function does not make any copies of the
+    // pointer argument that outlive the function itself.
+    in_arg->addAttr(llvm::Attribute::NoCapture);
+    // No aliasing is allowed on the pointer arguments
+    // for this function.
+    in_arg->addAttr(llvm::Attribute::NoAlias);
+
+    // Lookup the vector function.
+    auto vec_f = module->getFunction(name + ".vecargs");
+    assert(vec_f != nullptr);
+
+    // Create a new basic block to start insertion into.
+    auto *bb = llvm::BasicBlock::Create(get_context(), "entry", f);
+    assert(bb != nullptr);
+    builder->SetInsertPoint(bb);
+
+    // Init the variable for the start of the loop (i = 0).
+    auto start_val = builder->getInt32(0);
+    assert(start_val);
+
+    // Make the new basic block for the loop header,
+    // inserting after current block.
+    assert(builder->GetInsertBlock()->getParent() == f);
+    auto *preheader_bb = builder->GetInsertBlock();
+    auto *loop_bb = llvm::BasicBlock::Create(get_context(), "loop", f);
+
+    // Insert an explicit fall through from the current block to the loop_bb.
+    builder->CreateBr(loop_bb);
+
+    // Start insertion in loop_bb.
+    builder->SetInsertPoint(loop_bb);
+
+    // Start the PHI node with an entry for Start.
+    auto *variable = builder->CreatePHI(builder->getInt32Ty(), 2, "i");
+    variable->addIncoming(start_val, preheader_bb);
+
+    // Emit the body of the loop.
+    // First we need to compute the pointers corresponding to this
+    // loop iteration.
+    // The output pointer.
+    auto out_ptr = builder->CreateInBoundsGEP(
+        // The underlying type for the array.
+        builder->getDoubleTy(),
+        // The array (that is, the pointer argument passed
+        // to this function).
+        &*out_arg,
+        // The offset.
+        variable,
+        // Name for the pointer variable.
+        "out_ptr");
+
+    // The input pointer.
+    auto in_ptr = builder->CreateInBoundsGEP(
+        // The underlying type for the array.
+        builder->getDoubleTy(),
+        // The array (that is, the pointer argument passed
+        // to this function).
+        &*in_arg,
+        // The offset.
+        builder->CreateMul(variable, builder->getInt32(static_cast<std::uint32_t>(vars.size())), "in_offset"),
+        // Name for the pointer variable.
+        "in_ptr");
+
+    // Invoke the vector function, and store the result in out_ptr.
+    auto vec_f_call = builder->CreateCall(vec_f, {in_ptr}, "calltmp");
+    vec_f_call->setTailCall(true);
+    builder->CreateStore(vec_f_call, out_ptr);
+
+    // Compute the next value of the iteration.
+    auto *next_var = builder->CreateAdd(variable, builder->getInt32(1), "nextvar");
+
+    // Compute the end condition.
+    auto *end_cond = builder->CreateICmp(llvm::CmpInst::ICMP_ULT, next_var, builder->getInt32(batch_size), "loopcond");
+
+    // Create the "after loop" block and insert it.
+    auto *loop_end_bb = builder->GetInsertBlock();
+    auto *after_bb = llvm::BasicBlock::Create(get_context(), "afterloop", f);
+
+    // Insert the conditional branch into the end of loop_end_bb.
+    builder->CreateCondBr(end_cond, loop_bb, after_bb);
+
+    // Any new code will be inserted in after_bb.
+    builder->SetInsertPoint(after_bb);
+
+    // Add a new entry to the PHI node for the backedge.
+    variable->addIncoming(next_var, loop_end_bb);
+
+    // Finish off the function.
+    builder->CreateRetVoid();
+
+    // Verify it.
+    verify_function(f);
+
+    // Optimize it.
+    optimize_function(f);
+}
+
+void llvm_state::add_expression(const std::string &name, const expression &e, unsigned batch_size)
 {
     detail::check_symbol_name(name);
 
@@ -271,12 +413,23 @@ void llvm_state::add_expression(const std::string &name, const expression &e, bo
     // variables from the expression.
     const auto vars = e.get_variables();
 
-    add_varargs_expression(name, e, optimize, vars);
-    add_vecargs_expression(name, optimize, vars);
+    add_varargs_expression(name, e, vars);
+    add_vecargs_expression(name, vars);
+    add_batch_expression(name, vars, batch_size);
 }
 
 void llvm_state::compile()
 {
+    // TODO: read up:
+    // https://stackoverflow.com/questions/48300510/llvm-api-optimisation-run
+    llvm::legacy::PassManager pm;
+    llvm::PassManagerBuilder pm_builder;
+    pm_builder.OptLevel = 3;
+    pm_builder.DisableUnrollLoops = false;
+    pm_builder.Inliner = llvm::createFunctionInliningPass(50);
+    pm_builder.populateModulePassManager(pm);
+    pm.run(*module);
+
     jitter.add_module(std::move(module));
 }
 
@@ -288,7 +441,12 @@ std::uintptr_t llvm_state::jit_lookup(const std::string &name)
 
 llvm_state::f_ptr llvm_state::fetch(const std::string &name)
 {
-    return reinterpret_cast<double (*)(const double *)>(jit_lookup(name + ".vecargs"));
+    return reinterpret_cast<f_ptr>(jit_lookup(name + ".vecargs"));
+}
+
+llvm_state::f_batch_ptr llvm_state::fetch_batch(const std::string &name)
+{
+    return reinterpret_cast<f_batch_ptr>(jit_lookup(name + ".batch"));
 }
 
 void llvm_state::set_verify(bool f)
--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -12,37 +12,34 @@ using namespace lambdifier::literals;
 int main()
 {
     // Init the LLVM machinery.
-    lambdifier::llvm_state s{"my llvm module"};
+    lambdifier::llvm_state s{"my llvm module", 3};
 
     // Build an expression.
     auto c = 42_num;
     auto x = "x"_var, y = "y"_var;
 
-    auto ex = pow(sin(x) * sqrt(x), c);
+    auto ex = x + x - x * x;
     std::cout << ex << '\n';
 
-    s.add_expression("f", ex);
+    s.add_expression("f", ex, 10);
 
     std::cout << s.dump() << '\n';
-
-    return 0;
 
     // Compile all the functions in the module.
     s.compile();
 
+    return 0;
+
     // Fetch the compiled function.
     auto func1 = s.fetch("f");
-    auto func2 = s.fetch_vararg<2>("f");
+    auto func2 = s.fetch_vararg<1>("f");
+    auto func3 = s.fetch_batch("f");
 
     // Invoke it.
     double args[] = {3.45, 6.78};
-    std::cout << func1(args) << '\n';
-    std::cout << func1(args) << '\n';
-    std::cout << func1(args) << '\n';
-    std::cout << func1(args) << '\n';
-
-    std::cout << func2(args[0], args[1]) << '\n';
-    std::cout << func2(args[0], args[1]) << '\n';
-    std::cout << func2(args[0], args[1]) << '\n';
-    std::cout << func2(args[0], args[1]) << '\n';
+    double out[] = {0, 0};
+    std::cout << func1(args) << ", " << func1(args + 1) << '\n';
+    std::cout << func2(args[0]) << ", " << func2(args[1]) << '\n';
+    func3(out, args);
+    std::cout << out[0] << ", " << out[1] << '\n';
 }
